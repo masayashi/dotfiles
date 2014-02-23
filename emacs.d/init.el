0c7b0deb0f2ecd140ce8fdb052b46284a515e243
masayashi <masayashi@gmail.com>
2014-02-23 17:17:13 +0900

    いろいろ設定

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,8 +13,34 @@
 (package-initialize)
 
 ;; helm
-(require 'helm)
+(require 'helm-config)
 (helm-mode 1)
+
+;; redo
+(require 'redo+)
+
+;; 行と列の番号をモードラインに表示
+(column-number-mode t)
+
+;;-----------------------------------------------------------------------------
+;; 見た目まわり
+;;-----------------------------------------------------------------------------
+
+;; テーマ変更
+;(load-theme 'deeper-blue t)
+;(load-theme 'misterioso t)
+(load-theme 'tango-dark t)
+
+;; ツールバー、メニューバーの表示設定
+(tool-bar-mode 0)
+(menu-bar-mode 1)
+
+;;-----------------------------------------------------------------------------
+;; 基本設定
+;;-----------------------------------------------------------------------------
+
+;; 起動時のスプラッシュ画面を非表示
+(setq inhibit-startup-message t)
 
 ;; 文字コード
 (set-language-environment 'utf-8)
@@ -31,24 +57,85 @@
 ;; ビープ音を消す
 (setq visible-bell t)
 
-;; 起動時のスプラッシュ画面を非表示
-(setq inhibit-startup-message t)
-
-;; ツールバー、メニューバーの表示設定
-(tool-bar-mode 0)
-(menu-bar-mode 1)
-
-;; 対応する括弧をハイライト
-(show-paren-mode t)
-
 (recentf-mode t)
 
-;-----------------------------------------------------------------------
-;;
-(global-set-key "\C-o" 'next-multiframe-window)
-(global-set-key "\C-h" 'delete-backward-char)
-(global-set-key "\C-z" 'undo)
+;; カレントファイルのフルパスをタイトルバーに表示する
+(setq frame-title-format "%f")
 
+;; 対応する括弧をハイライト
+(setq show-paren-delay 0) ; 表示までの秒数を変更
+(show-paren-mode t)
+;(setq show-paren-style 'expression) ; 式内を強調表示
+;(set-face-background 'show-paren-match-face nil)
+;(set-face-underline-p 'show-paren-match-face "yellow")
+
+
+;; バックアップファイルの作成場所を変更する
+;(setq backup-directory-alist
+;  (cons (cons ".*" (expand-file-name "~/.emacs.d/backup"))
+;        backup-directory-alist))
+;; バックアップとオートセーブファイルの作成場所を変更する
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backup/"))
+(setq auto-save-file-name-transforms
+  `((".*", (expand-file-name "~/.emacs.d/backup/") t)))
+
+;; c-k のときに改行を一緒に行う
+(setq kill-whole-line t)
+
+
+ ;;-----------------------------------------------------------------------------
+ ;; フォント設定
+ ;;-----------------------------------------------------------------------------
+ ;; 英語
+(set-face-attribute 'default nil
+                    :family "Lucida Console"
+                    :height 100)
+;; 日本語
+(set-fontset-font
+ nil 'japanese-jisx0208
+ (font-spec :family "Meiryo"))
+(setq face-font-rescale-alist
+      '((".*Lucida Console.*" . 1.0)))
+
+;; 現在行のハイライト
+(defface my-hl-line-face
+  '((((class color) (background dark))
+     (:background "brown" t))
+    (((class color) (background light))
+     (:background "LightGoldenrodYellow" t))
+    (t (:bold t)))
+  "hl-line's my face")
+(setq hl-line-face 'my-hl-line-face)
+(global-hl-line-mode t)
+
+;;-----------------------------------------------------------------------------
+;; キーバインド
+;;-----------------------------------------------------------------------------
+(define-key global-map (kbd "C-o") 'other-window)
+(define-key global-map (kbd "C-h") 'delete-backward-char)
+(define-key global-map (kbd "C-z") 'undo)
+(define-key global-map (kbd "C-;") 'helm-mini)
+(define-key global-map (kbd "C-m") 'newline-and-indent)
+(define-key global-map (kbd "C-S-z") 'redo)
+
+(define-key global-map (kbd "M-g") 'goto-line)
+
+(define-key global-map (kbd "M-x") 'helm-M-x)
+(define-key global-map (kbd "M-y") 'helm-show-kill-ring)
+
+(define-key helm-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-find-files-map (kbd "C-h") 'delete-backward-char)
+(define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
+(define-key helm-find-file-map (kbd "TAB") 'helm-execute-persistent-action)
+
+;;-----------------------------------------------------------------------------
+;; メモ
+;;-----------------------------------------------------------------------------
+; helm-for-files
+; helm-show-kill-ring
+; helm-resume
+; helm-buffers-list
 
 
 
